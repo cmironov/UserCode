6121d14f4a308976f41c582861306fa9dd3a42e3
mironov <>
2012-03-27 14:16:57 +0000

    fix compilation erros for stuff in plugin dir

--- hiMuonRegit/setHiMuonRegit.sh
+++ hiMuonRegit/setHiMuonRegit.sh
@@ -2,7 +2,7 @@
 
 # muon specific part
 cvs co -r V00-02-04 RecoHI/HiMuonAlgos
-
+cvs co -r $CMSSW_VERSION RecoHi/HiMuonAlgos/plugin
 #Either work in CMSSW_4_4_4 or higher, or check out the following tag for older 4_4_X releases
 cvs co -r V01-05-03 RecoHI/HiTracking
 
