ff263eef1d091a905aadad51edf4942d9ea892fa
mironov <>
2012-03-27 09:52:14 +0000

    example of skimming and running regit

--- hiMuonRegit/runMuonRegit.py
+++ hiMuonRegit/runMuonRegit.py
@@ -7,41 +7,51 @@ process.load("Configuration.StandardSequences.ReconstructionHeavyIons_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 
+process.load('Configuration.EventContent.EventContentHeavyIons_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 #process.GlobalTag.globaltag = 'GR_R_44_V10::All'
-process.GlobalTag.globaltag      = 'GR_P_V27A::All'
-#process.GlobalTag.globaltag = 'STARTHI44_V7::All'
+#process.GlobalTag.globaltag      = 'GR_P_V27A::All'
+process.GlobalTag.globaltag = 'STARTHI44_V7::All'
 
 
 ##################################################################################
 # setup 'standard'  options
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1)
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10)
                                        )
 
 # Input source
 process.source = cms.Source("PoolSource",
-                            fileNames = cms.untracked.vstring("file:/tmp/camelia/hiReco_RAW2DIGI_RECO_479_1_Qkt.root"),
+                            fileNames = cms.untracked.vstring("file:/tmp/camelia/mix_jpsi69.root"),
                             noEventSort = cms.untracked.bool(True),
                             duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
                             skipEvents=cms.untracked.uint32(0),
-                            inputCommands = cms.untracked.vstring('keep *',
-                                                                  'drop *_muons*_*_RECO',
-                                                                  'drop *_globalMuons_*_RECO'
-                                                                  )
+                            
+
                             )
 # Output file
 process.output = cms.OutputModule("PoolOutputModule",
                                   splitLevel = cms.untracked.int32(0),
-                                  outputCommands = cms.untracked.vstring('drop *',
-                                                                         'keep recoTracks_hiRegitMuGeneralTracks_*_*'
-                                                                         'keep *_remuons*_*_*',
-                                                                         'drop *_reglobalMuons_*_*'
-                                                                         ),
-                                  fileName = cms.untracked.string('/tmp/camelia/regitStd_bjpsi69Gun_v2.root')
+                                  outputCommands = process.RECOSIMEventContent.outputCommands,
+                                 # outputCommands = process.FEVTDEBUGHLTEventContent.outputCommands,
+                                  fileName = cms.untracked.string('/tmp/camelia/regitTest_mc_outputTest.root')
                                   )
-
+process.output.outputCommands.extend(cms.untracked.vstring(#'keep *',
+                                                           'keep *_hiRegitMuGeneralTracks*_*_*',
+                                                           'keep *_remuons*_*_*',
+                                                           'keep *_reglobalMuons*_*_*',
+                                                           'keep *_retev*_*_*',
+                                                           'keep *_recalomuons*_*_*',
+                                                           #'keep *_hiRegitMu*_*_*',
+                                                          # 'keep *_hi*Step*_*_*',
+                                                          # 'keep *_hiSecondPixelTripletGlobalPrimTracks_*_*',
+                                                          # 'keep *_hiPixelPairGlobalPrimTracks_*_*',
+                                                          # 'keep *_hiGeneralTracks_*_*',
+                                                           'keep *_*Tracks_*_*',
+                                                          # 'keep *_hiGenParticles_*_*'
+                                                           ))
 ##################################################################################
 # Some Services
+'''
 process.MessageLogger = cms.Service("MessageLogger",
                                     cout = cms.untracked.PSet(default = cms.untracked.PSet(limit = cms.untracked.int32(0)
                                                                                            )
@@ -56,7 +66,7 @@ process.SimpleMemoryCheck = cms.Service('SimpleMemoryCheck',
 
 process.Timing = cms.Service("Timing")
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
-
+'''
 
 ##################################################################################
 # Schedule definition
@@ -77,11 +87,4 @@ process.endjob_step    = cms.Path(process.endOfProcess)
 process.schedule       = cms.Schedule(process.trackerRecHits,process.hiTrackReco,process.muRegit)
 process.schedule.extend([process.endjob_step,process.out_step])
 
-
-# replace everywhere the names of the new muon collections 
-from PhysicsTools.PatAlgos.tools.helpers import massSearchReplaceAnyInputTag
-massSearchReplaceAnyInputTag(process.patMuonSequence, 'muons','remuons')
-massSearchReplaceAnyInputTag(process.patMuonSequence, 'globalMuons','reglobalMuons')
-massSearchReplaceAnyInputTag(process.outOnia2MuMu, 'hiSelectedTracks','hiRegitMuGeneralTracks')
-massSearchReplaceAnyInputTag(process.outTnP, 'hiSelectedTracks','hiRegitMuGeneralTracks')
 
---
+++ hiMuonRegit/setHiMuonRegit.sh
@@ -0,0 +1,10 @@
+# script to set the muon regit
+
+# muon specific part
+cvs co -r V00-02-04 RecoHI/HiMuonAlgos
+
+#Either work in CMSSW_4_4_4 or higher, or check out the following tag for older 4_4_X releases
+cvs co -r V01-05-03 RecoHI/HiTracking
+
+
+scram b -j4
