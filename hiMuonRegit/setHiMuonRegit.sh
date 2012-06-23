50d9b5e39f3b0d2c3b1e5bc34fe22fb6a28ea54b
mironov <>
2012-06-23 09:15:51 +0000

    some comments to be clear what was changed

--- hiMuonRegit/setHiMuonRegit.sh
+++ hiMuonRegit/setHiMuonRegit.sh
@@ -5,20 +5,25 @@
 
 #------------ for first path through real data
 cvs co -r $CMSSW_VERSION RecoHI/HiMuonAlgos
+
+# added on June 23 ------
 cvs co -r $CMSSW_VERSION RecoMuon/MuonIdentification
 cvs co -r $CMSSW_VERSION RecoMuon/GlobalTrackingTools
-
+# end of adding
 
 # --- from camelia's user's area
 cvs co UserCode/CMironov/hiMuonRegit
 cp UserCode/CMironov/hiMuonRegit/HiRegitMuon*.py RecoHI/HiMuonAlgos/python
 cp UserCode/CMironov/hiMuonRegit/hiMuonIterativeTk_cff.py RecoHI/HiMuonAlgos/python
 cp UserCode/CMironov/hiMuonRegit/HiReRecoMuon_cff.py RecoHI/HiMuonAlgos/python
-cp UserCode/CMironov/hiMuonRegit/HIMuonTrackingRegionProducer.h RecoHI/HiMuonAlgos/plugins
 
+# added on June 23rd
+cp UserCode/CMironov/hiMuonRegit/HIMuonTrackingRegionProducer.h RecoHI/HiMuonAlgos/plugins
 cp UserCode/CMironov/hiMuonRegit/RecoMuon/MuonIdProducer.cc RecoMuon/MuonIdentification/plugins
 cp UserCode/CMironov/hiMuonRegit/RecoMuon/MuonIdProducer.h RecoMuon/MuonIdentification/plugins
 cp UserCode/CMironov/hiMuonRegit/RecoMuon/MuonTrackingRegionBuilder.cc RecoMuon/GlobalTrackingTools/src
+# end of adding
+
 
 #Either work in CMSSW_4_4_4 or higher, or check out the following tag for older 4_4_X releases
 cvs co -r V01-05-03 RecoHI/HiTracking
