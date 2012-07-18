1175893a6559baff6b5e26605cc6627f9261071a
dmoon <>
2012-07-18 21:07:18 +0000

    *** empty log message ***

--- hin-12-014/macros/rootlogon.C
+++ hin-12-014/macros/rootlogon.C
@@ -3,7 +3,7 @@
   gStyle->SetPalette(1);
   gStyle->SetOptTitle(0);
   gStyle->SetOptStat(0);
-  gStyle->SetOptFit(1);
+  gStyle->SetOptFit(0);
 
   gStyle->SetTitleFillColor(0);
   gStyle->SetStatColor(0);
@@ -25,13 +25,13 @@
   gStyle->SetLabelFont(42,"xyz");
   gStyle->SetTitleFont(42,"xyz");
   gStyle->SetTitleSize(0.048,"xyz");
-  gStyle->SetPadBottomMargin(0.14);
+  gStyle->SetPadBottomMargin(0.12);
   gStyle->SetPadTopMargin(0.03);
-  gStyle->SetPadRightMargin(0.15);
-  gStyle->SetPadLeftMargin(0.15);
+  gStyle->SetPadRightMargin(0.065);
+  gStyle->SetPadLeftMargin(0.12);
 
-  gStyle->SetTitleXOffset(1.2);
-  gStyle->SetTitleYOffset(1.4);
+  gStyle->SetTitleXOffset(1.15);
+  gStyle->SetTitleYOffset(1.2);
 
   gStyle->SetPadTickX(1);
   gStyle->SetPadTickY(1);
@@ -41,24 +41,11 @@
 
   gStyle->SetHistMinimumZero(kTRUE);
 
-  //gStyle->SetErrorX(0); // disable if you want to draw horizontal error bars, e.g. when having variable bin size
-  //gStyle->SetEndErrorSize(0);
+  gStyle->SetErrorX(0); // disable if you want to draw horizontal error bars, e.g. when having variable bin size
+  gStyle->SetEndErrorSize(0);
 
   gStyle->SetMarkerStyle(20);
   gStyle->SetMarkerSize(0.8);
-
-  const Int_t NRGBs = 5;
-  //const Int_t NCont = 255;
-  const Int_t NCont = 200;
-
-  Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
-  Double_t red[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 0.51 };
-  Double_t green[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
-  Double_t blue[NRGBs]  = { 0.51, 1.00, 0.12, 0.00, 0.00 };
-  TColor::CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
-  //gStyle->CreateColorGradientTable(NRGBs, stops, red, green, blue, NCont);
-
-  gStyle->SetNumberContours(NCont);
 
   gROOT->ForceStyle();
 }
