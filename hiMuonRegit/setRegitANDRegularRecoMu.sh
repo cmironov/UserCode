3875e84c62c75a7c75e67117339cc31f13845cb7
mironov <>
2012-12-06 10:23:46 +0000

    fix for crushes in 445p1 with versions made to work in 6xx

--- hiMuonRegit/setRegitANDRegularRecoMu.sh
+++ hiMuonRegit/setRegitANDRegularRecoMu.sh
@@ -1,8 +1,19 @@
 #
 # !!!!! have to be in CMSSW_4_4_5_patch1 !!!!!!
 
-cvs co -r V03-00-00 RecoHI/HiMuonAlgos                
-cvs co -r V00-00-20 RecoHI/Configuration
+cvs co -r $CMSSW_VERSION RecoHI/HiMuonAlgos                
+cvs co -r $CMSSW_VERSION RecoHI/Configuration
+cvs co -r $CMSSW_VERSION Configuration/StandardSequences
+
+cvs co -d runBothRegitRegular UserCode/CMironov/hiMuonRegit/runBothRegitRegular
+
+cp runBothRegitRegular/HiReRecoMuon_cff.py RecoHI/HiMuonAlgos/python
+cp runBothRegitRegular/RecoHiMuon_EventContent_cff.py RecoHI/HiMuonAlgos/python
+
+cp runBothRegitRegular/Reconstruction_HI_cff.py RecoHI/Configuration/python
+cp runBothRegitRegular/ReconstructionHeavyIons_cff.py Configuration/StandardSequences/python
+
+rm -rf runBothRegitRegular
 
 scram b -j4
 
