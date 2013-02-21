2d4355f0d61b1364e8d7bba675db09e77df3ab23
mironov <>
2013-02-21 12:28:10 +0000

    latest changes for centrality

--- oniaIn442/setOnia_538x.sh
+++ oniaIn442/setOnia_538x.sh
@@ -3,14 +3,10 @@ cvs co -r V01-06-05 RecoVertex/PrimaryVertexProducer
 
 cvs co -r pPbProd_v04 DataFormats/HeavyIonEvent
 cvs co -r pPbProd_v07 HeavyIonsAnalysis/Configuration
+
 cvs co -r pPbProd_v06 RecoHI/HiCentralityAlgos
 cvs co -d Appeltel/RpPbAnalysis UserCode/Appeltel/RpPbAnalysis
 
 cvs co -r branch_pA -d HiSkim/HiOnia2MuMu UserCode/tdahms/HiSkim/HiOnia2MuMu
 cvs co -r branch_pA -d HiAnalysis/HiOnia UserCode/tdahms/HiAnalysis/HiOnia
 
-cvs co -r HEAD DataFormats/Math/interface/VDTMath.h
-cvs co -r V01-15-01 MuonAnalysis/MuonAssociators
-
-cvs co UserCode/CMironov/oniaIn442
-cp UserCode/CMironov/oniaIn442/VDTMath.h DataFormats/Math/interface/VDTMath.h
